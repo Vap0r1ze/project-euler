0f50b726b7d32f8d3891255cbb1a88e2855c29c9
Vap0r1ze <superdash993@gmail.com>
2018-10-20 02:03:17 -0400

    .

--- 10.js
+++ 10.js
@@ -1,10 +1,10 @@
-let numbers = []
+const numbers = []
 let sum = 0
 
-for(let i = 2; i < 2e6; i++) {
+for (let i = 2; i < 2e6; i++) {
   if (!(numbers[i] === false)) {
     sum += i
-    for(let p = i; p < 2e6; p += i) {
+    for (let p = i; p < 2e6; p += i) {
       numbers[p] = false
     }
   }
