43504d833ba552fe82dfc1a1b8be8edc77dd80ff
Vap0r1ze <superdash993@gmail.com>
2018-10-20 02:02:35 -0400

    10 optimized (thx esja)

--- 10.js
+++ 10.js
@@ -1,12 +1,13 @@
-const { isPrime } = require('./helpers')
+let numbers = []
+let sum = 0
 
-let sum = 2
-let n = 3
-
-while (n < 2e6) {
-  if (isPrime(n))
-    sum += n
-  n += 2
+for(let i = 2; i < 2e6; i++) {
+  if (!(numbers[i] === false)) {
+    sum += i
+    for(let p = i; p < 2e6; p += i) {
+      numbers[p] = false
+    }
+  }
 }
 
 exports.value = sum
